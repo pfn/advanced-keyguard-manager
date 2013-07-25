4c96db2c586b430b53db5e16c6a1dfb077ad936c
Perry Nguyen <pfnguyen@hanhuy.com>
2013-07-25 08:18:19 -0700

    Reset passwordSetRecently on call, notif changes

--- src/com/hanhuy/android/bluetooth/keyguard/LockMediator.java
+++ src/com/hanhuy/android/bluetooth/keyguard/LockMediator.java
@@ -162,6 +162,8 @@ public class LockMediator {
     // there is a window of opportunity where a settings-changed password
     // reset will mess this up
     public boolean passwordSetRecently() {
-        return System.currentTimeMillis() - changeTime < 10000;
+        long t = changeTime;
+        changeTime = 0;
+        return System.currentTimeMillis() - t < 60000;
     }
 }
