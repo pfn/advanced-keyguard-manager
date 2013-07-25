d27fc985559a7670d70e44cfb93c920a68da2ffd
Perry Nguyen <pfnguyen@hanhuy.com>
2013-07-25 10:54:26 -0700

    Register for BOOT_COMPLETED; update state on boot
    Use common NotifyStateChangeReceiver

--- src/com/hanhuy/android/bluetooth/keyguard/NotifyStateChangeReceiver.java
+++ src/com/hanhuy/android/bluetooth/keyguard/NotifyStateChangeReceiver.java
@@ -4,7 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-public class ConnectivityChangeReceiver extends BroadcastReceiver {
+public class NotifyStateChangeReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         LockMediator.getInstance(context).notifyStateChanged();
     }
